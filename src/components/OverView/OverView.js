548685b8cf61021edea18e48b1b4a53f08e062c7
Hasibul0211 <mdhasibul0211@gmail.com>
2022-12-20 03:17:49 +0600

    commit

--- src/components/OverView/OverView.js
+++ src/components/OverView/OverView.js
@@ -3,25 +3,27 @@ import './OverView.css';
 import PieChart from '../Shared/PieChart/PieChart';
 const OverView = () => {
     return (
-        <div>
+        <div style={{ height: '100vh' }}>
             {/* first cont section here */}
             <section className='firstCont'>
                 {/* total student section here */}
-                <div className=''>
-                    {/* <PieChart></PieChart> */}
+                <div className='TsCountT'>
                     <h3>Running Total Students</h3>
                 </div>
                 <div className='TsCountD'>
-
                     <h1>21</h1>
                 </div>
 
             </section>
-            <section style={{ width: '100%' }}>
-                <div className='TsCountD'>
-                    <h3>Current Payment Remain</h3>
-                    <h1>21</h1>
+            <section className='firstCont'>
+                {/* total student section here */}
+                <div className='TsCountT'>
+                    <h3>Payment Dues</h3>
                 </div>
+                <div className='TsCountD'>
+                    <h1>00</h1>
+                </div>
+
             </section>
 
         </div>
