b6f7bed5b5e2a79d0dd2971a357b0bffe3beb8d2
Hasibul0211 <mdhasibul0211@gmail.com>
2023-02-02 23:30:36 +0600

    commit

--- src/components/Teachers/Teachers.js
+++ src/components/Teachers/Teachers.js
@@ -12,29 +12,96 @@ const Teachers = () => {
                         <img src={profileImg} alt="" style={{ width: '100%' }} />
                     </div>
                     <p className='teacherName'>pranto majumder</p>
+                    <p className='teacherQlt'>Qualification: Hon's First Year</p>
+                    <p className='teacherSubs'>subjects: </p>
+                    <p className='teacherSub'>Four & Five All subjects</p>
+                    <p className='teacherSub'>HSC: Higher Math</p>
+                    <p className='teacherPhn'>Contact: 01900000000</p>
+                    <p className='teacherClass'>Class taken total: 05</p>
                 </div>
-
-
-
-
 
 
                 <div className='teacherBox'>
-                    <p>sohel</p>
+                    <div className='teacherImg'>
+                        <img src={profileImg} alt="" style={{ width: '100%' }} />
+                    </div>
+                    <p className='teacherName'>Sohel biswas</p>
+                    <p className='teacherQlt'>Qualification: Hon's(Chemistry)</p>
+                    <p className='teacherSubs'>subjects: </p>
+                    <p className='teacherSub'>Chemistry & English</p>
+                    <p className='teacherPhn'>Contact: 01900000000</p>
+                    <p className='teacherClass'>Class taken total: 05</p>
                 </div>
-                <div className='teacherBox'>
-                    <p>masum</p>
-                </div>
-                <div className='teacherBox'>
-                    <p>shanto</p>
-                </div>
-                <div className='teacherBox'>
-                    <p>raj</p>
-                </div>
-                <div>
 
-                    B
+                <div className='teacherBox'>
+                    <div className='teacherImg'>
+                        <img src={profileImg} alt="" style={{ width: '100%' }} />
+                    </div>
+                    <p className='teacherName'>Masum Hasan</p>
+                    <p className='teacherQlt'>Qualification: Hon's(psycology)</p>
+                    <p className='teacherSubs'>subjects: </p>
+                    <p className='teacherSub'>Bangladesh & Global science</p>
+                    <p className='teacherPhn'>Contact: 01900000000</p>
+                    <p className='teacherClass'>Class taken total: 05</p>
                 </div>
+
+
+                <div className='teacherBox'>
+                    <div className='teacherImg'>
+                        <img src={profileImg} alt="" style={{ width: '100%' }} />
+                    </div>
+                    <p className='teacherName'>mubarok hossain shanto</p>
+                    <p className='teacherQlt'>Qualification: Hon's(English)</p>
+                    <p className='teacherSubs'>subjects: </p>
+                    <p className='teacherSub'>English</p>
+                    <p className='teacherPhn'>Contact: 01900000000</p>
+                    <p className='teacherClass'>Class taken total: 05</p>
+                </div>
+                <div className='teacherBox'>
+                    <div className='teacherImg'>
+                        <img src={profileImg} alt="" style={{ width: '100%' }} />
+                    </div>
+                    <p className='teacherName'>Raj</p>
+                    <p className='teacherQlt'>Qualification: Hon's(Accounting)</p>
+                    <p className='teacherSubs'>subjects: </p>
+                    <p className='teacherSub'>Accounting</p>
+                    <p className='teacherPhn'>Contact: 01900000000</p>
+                    <p className='teacherClass'>Class taken total: 05</p>
+                </div>
+                <div className='teacherBox'>
+                    <div className='teacherImg'>
+                        <img src={profileImg} alt="" style={{ width: '100%' }} />
+                    </div>
+                    <p className='teacherName'>nizam uddin nahid</p>
+                    <p className='teacherQlt'>Qualification: Hon's(Aggriculture)</p>
+                    <p className='teacherSubs'>subjects: </p>
+                    <p className='teacherSub'>Ict(HSC),Englsih,General Science, Religion</p>
+                    <p className='teacherPhn'>Contact: 01900000000</p>
+                    <p className='teacherClass'>Class taken total: 05</p>
+                </div>
+                <div className='teacherBox'>
+                    <div className='teacherImg'>
+                        <img src={profileImg} alt="" style={{ width: '100%' }} />
+                    </div>
+                    <p className='teacherName'>Mehedi Hasan</p>
+                    <p className='teacherQlt'>Qualification: Hon's(Bangla)</p>
+                    <p className='teacherSubs'>subjects: </p>
+                    <p className='teacherSub'>Biology(HSC+SSC), Bangla</p>
+                    <p className='teacherPhn'>Contact: 01900000000</p>
+                    <p className='teacherClass'>Class taken total: 05</p>
+                </div>
+                <div className='teacherBox'>
+                    <div className='teacherImg'>
+                        <img src={profileImg} alt="" style={{ width: '100%' }} />
+                    </div>
+                    <p className='teacherName'>Md Hasibul Hasan</p>
+                    <p className='teacherQlt'>Qualification: Bsc(EEE)</p>
+                    <p className='teacherSubs'>subjects: </p>
+                    <p className='teacherSub'>Physics,Chemistry(HSC+SSC), Math</p>
+                    <p className='teacherPhn'>Contact: 01906899680</p>
+                    <p className='teacherClass'>Class taken total: 05</p>
+                </div>
+
             </section>
         </div>
     );
